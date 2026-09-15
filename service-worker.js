5785dd2d38c1831546ff8c416162231e7301ee30
NicoCort07 <nicocortesroldan@gmail.com>
2026-09-16 00:39:58 +0200

    Añadir uso sin conexión

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+const CACHE = "mis-finanzas-v1";
+const ARCHIVOS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+self.addEventListener("install", evento => evento.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))));
+self.addEventListener("activate", evento => evento.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", evento => evento.respondWith(caches.match(evento.request).then(respuesta => respuesta || fetch(evento.request))));
+
